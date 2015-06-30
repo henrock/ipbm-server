d04450e0a054beb5d8deef4704bd6fdd63bb7e89
whckmstrjck <tobias.mattsson@gmail.com>
2015-06-30 22:07:39 +0200

    MissileLauncher Critical error fix

--- MissileLauncher.py
+++ MissileLauncher.py
@@ -5,14 +5,14 @@ class MissileLauncher:
 	parentBody = None
 	
 	def launchMissle(self):
-	missile = new BodyOfMass()
-	missile.mass = 1
-	missile.missileLauncher = self
-	missile.position = self.parentBody.position
-	missile.velocity = self.launchingVelocity
-	missile.acceleration = {'x': 0, 'y': 0}
-	missile.radius = 0
-	missile.name = None
-	missile.isControlled = False
-	
-	return missile
+		missile = new BodyOfMass()
+		missile.mass = 1
+		missile.missileLauncher = self
+		missile.position = self.parentBody.position
+		missile.velocity = self.launchingVelocity
+		missile.acceleration = {'x': 0, 'y': 0}
+		missile.radius = 0
+		missile.name = None
+		missile.isControlled = False
+		
+		return missile
