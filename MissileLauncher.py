9a4b98f65794fa1008362e8dee844bac002000c3
whckmstrjck <tobias.mattsson@gmail.com>
2015-06-30 22:02:00 +0200

    MissileLauncher

--- MissileLauncher.py
+++ MissileLauncher.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
 
 class MissileLauncher:
-	launchingVelocity = {x, y}
+	launchingVelocity = {'x': 10, 'y': 10}
 	parentBody = None
 	
 	def launchMissle(self):
+	missile = new BodyOfMass()
+	missile.mass = 1
+	missile.missileLauncher = self
+	missile.position = self.parentBody.position
+	missile.velocity = self.launchingVelocity
+	missile.acceleration = {'x': 0, 'y': 0}
+	missile.radius = 0
+	missile.name = None
+	missile.isControlled = False
 	
+	return missile
