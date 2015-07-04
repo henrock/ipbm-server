b2868bd6be249cc765bbb1a96d458853e8a073a3
tobias-mattsson <trunroswi@gmail.com>
2015-07-05 00:34:02 +0200

    import BodyOfMass gjordes inte fixat stavfel i metodnamn lade till att missilen skapas en bit från planeten så det inte bli konstig fysik på en gång när den skapas

--- MissileLauncher.py
+++ MissileLauncher.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
+import BodyOfMass
 
 class MissileLauncher:
-	launchingVelocity = {'x': 10, 'y': 10}
-	parentBody = None
-	
-	def launchMissle(self):
-		missile = BodyOfMass()
-		missile.mass = 1
-		missile.missileLauncher = self
-		missile.position = self.parentBody.position
-		missile.velocity = self.launchingVelocity
-		missile.acceleration = {'x': 0, 'y': 0}
-		missile.radius = 0
-		missile.name = None
-		missile.isControlled = False
-		
-		return missile
+    launchingVelocity = {'x': 10, 'y': 10}
+    parentBody = None
+
+    def launchMissile(self):
+        missile = BodyOfMass.BodyOfMass()
+        missile.mass = 1
+        missile.missileLauncher = self
+        missile.position['x'] = self.parentBody.position['x'] + self.launchingVelocity['x'] # förhindra instakrock
+        missile.position['y'] = self.parentBody.position['y'] + self.launchingVelocity['y'] # förhindra instakrock
+        missile.velocity = self.launchingVelocity
+        missile.acceleration = {'x': 0, 'y': 0}
+        missile.radius = 1
+        missile.name = None
+        missile.isControlled = False
+        return missile
